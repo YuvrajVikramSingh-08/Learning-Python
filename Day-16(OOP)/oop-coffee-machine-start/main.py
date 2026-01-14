966ebea31e234742dcbc9133cf02e4a98f17d03e
YuvrajVikramSingh-08 <yvstechandedu666@gmail.com>
2026-01-15 00:56:26 +0530

    OOP day1 complete

--- Day-16(OOP)/oop-coffee-machine-start/main.py
+++ Day-16(OOP)/oop-coffee-machine-start/main.py
@@ -3,4 +3,20 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 coffeemaker = CoffeeMaker()
-coffeemaker.report()
+moneymachine = MoneyMachine()
+menu = Menu()
+
+serve = True
+while serve:
+    choice = input("Which coffee would you like?(espresso/latte/cappuccino): ")
+    if choice == "off":
+        serve = False
+        print("Bye!")
+    elif choice == "report":
+        coffeemaker.report()
+        moneymachine.report()
+    elif menu.find_drink(choice):
+        order = menu.find_drink(choice)
+        if coffeemaker.is_resource_sufficient(order):
+            if moneymachine.make_payment(order.cost):
+                coffeemaker.make_coffee(order)
