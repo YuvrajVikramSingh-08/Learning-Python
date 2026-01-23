31c6d094d20127c7bc418d20f29f4347282d73c7
YuvrajVikramSingh-08 <yvstechandedu666@gmail.com>
2026-01-23 23:19:00 +0530

    Added highscore mechanism

--- Snake-Game/scoreboard.py
+++ Snake-Game/scoreboard.py
@@ -7,20 +7,23 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.hideturtle()
         self.penup()
         self.goto(x=0, y=270)
         self.color("white")
-        self.score_board()
+        self.update_scoreboard()
 
-    def score_board(self):
+    def update_scoreboard(self):
         self.clear()
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(arg=f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.score_board()
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="Game Over", align=ALIGNMENT, font=FONT)
+    def reset_board(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_scoreboard()
