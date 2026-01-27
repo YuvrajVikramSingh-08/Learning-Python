7362030e3d31ef027194c98e851a1203d49d52ec
YuvrajVikramSingh-08 <yvstechandedu666@gmail.com>
2026-01-28 00:34:03 +0530

    Completed python day 29 and made a password manager using tkinter.

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -1,17 +1,47 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_pass():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letter = [random.choice(letters) for char in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for char in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for char in range(nr_numbers)]
+
+    password_list = password_letter + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+
+    password_ = "".join(password_list)
+    pyperclip.copy(password_)
+    pass_ent.insert(0, password_)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def add_pass():
     website = web_ent.get()
     email = email_ent.get()
     password = pass_ent.get()
-    form = f"{website} | {email} | {password}\n"
-    with open("data.txt", "a") as data:
-        data.write(form)
-        web_ent.delete(0, END)
-        pass_ent.delete(0, END)
+
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="OOPS", message="Please don't leave any field empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
+                                                  f"\nPassword: {password} \nIs it ok to Save? ")
+        if is_ok:
+            form = f"{website} | {email} | {password}\n"
+            with open("data.txt", "a") as data:
+                data.write(form)
+                web_ent.delete(0, END)
+                pass_ent.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -46,7 +76,7 @@ pass_ent = Entry(width=20)
 pass_ent.grid(row=3, column=1)
 
 #buttons
-gen_b = Button(text="Generate Password", width=13)
+gen_b = Button(text="Generate Password", width=13, command=generate_pass)
 add_b = Button(text="Add", width=36, command = add_pass)
 
 gen_b.grid(row=3, column=2)
